bddb014c4f348ec9ca5035699178fd4092aa4b56
Alysson Narloch <alysson.narloch@gmail.com>
2015-12-08 23:59:35 -0200

    Enviando DUMP

---
+++ Dump20151208.sql
@@ -0,0 +1,286 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: main_app
+-- ------------------------------------------------------
+-- Server version	5.6.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Alysson Narloch','123456');
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchases`
+--
+
+DROP TABLE IF EXISTS `purchases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `start_destination_id` int(11) NOT NULL,
+  `end_destination_id` int(11) NOT NULL,
+  `start_at` datetime NOT NULL,
+  `end_at` datetime NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` int(11) NOT NULL,
+  `account` int(11) NOT NULL,
+  `agency` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_client_idx` (`client_id`),
+  CONSTRAINT `fk_client` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchases`
+--
+
+LOCK TABLES `purchases` WRITE;
+/*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-08 23:57:36
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: flight_company
+-- ------------------------------------------------------
+-- Server version	5.6.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city_name` varchar(100) NOT NULL,
+  `airport_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES (1,'São Paulo','Campinas'),(2,'Rio de Janeiro','Galeão'),(3,'Baia Maré Aeroporto','Baía Maré'),(4,'Fortaleza','Pinto Martins'),(5,'Maceió','Palmares'),(6,'Uberlândia','Tn Cl Av C. Bombonato'),(7,'Recife','Guararapes'),(8,'Foz do Iguaçú','Cataratas Ltnl'),(9,'Paraíba','João Suassuna'),(10,'Curitiba','Afonso Pena'),(11,'Papanduva','Unknow Airport');
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchases`
+--
+
+DROP TABLE IF EXISTS `purchases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `schedule_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `account` int(11) NOT NULL,
+  `agency` int(11) NOT NULL,
+  `end_at` datetime DEFAULT NULL,
+  `end_destination_id` int(11) DEFAULT NULL,
+  `start_at` datetime DEFAULT NULL,
+  `start_destination_id` int(11) DEFAULT NULL,
+  `client_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `schedule_fk_idx` (`schedule_id`),
+  KEY `FK_4g2dwhnlswa8jbm1dm1xktdt9` (`client_id`),
+  CONSTRAINT `FK_4g2dwhnlswa8jbm1dm1xktdt9` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+  CONSTRAINT `schedule_fk` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchases`
+--
+
+LOCK TABLES `purchases` WRITE;
+/*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
+INSERT INTO `purchases` VALUES (1,1,1000.00,1,'2015-10-25 16:25:21',0,0,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedules`
+--
+
+DROP TABLE IF EXISTS `schedules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_at` datetime NOT NULL,
+  `end_at` datetime NOT NULL,
+  `price` decimal(15,2) NOT NULL,
+  `total_seats` int(11) NOT NULL,
+  `available_seats` int(11) NOT NULL,
+  `start_destination_id` int(11) NOT NULL,
+  `end_destination_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `destination_fk_idx` (`start_destination_id`),
+  KEY `end_destination_fk_idx` (`end_destination_id`),
+  CONSTRAINT `end_destination_fk` FOREIGN KEY (`end_destination_id`) REFERENCES `cities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `start_destination_fk` FOREIGN KEY (`start_destination_id`) REFERENCES `cities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedules`
+--
+
+LOCK TABLES `schedules` WRITE;
+/*!40000 ALTER TABLE `schedules` DISABLE KEYS */;
+INSERT INTO `schedules` VALUES (1,'2015-11-01 08:22:00','2015-11-01 09:29:00',145.96,222,222,1,1),(2,'2015-11-01 10:45:00','2015-11-01 11:59:00',145.96,222,222,1,1),(3,'2015-11-01 12:18:00','2015-11-01 13:01:00',145.96,222,222,1,1),(4,'2015-11-01 14:12:00','2015-11-01 15:55:00',145.96,222,222,1,1),(5,'2015-11-01 16:36:00','2015-11-01 18:03:00',145.96,222,222,1,1),(6,'2015-11-01 08:22:00','2015-11-01 09:29:00',244.19,222,222,2,1),(7,'2015-11-01 10:45:00','2015-11-01 11:59:00',244.19,222,222,2,1),(8,'2015-11-01 12:18:00','2015-11-01 13:01:00',244.19,222,222,2,1),(9,'2015-11-01 14:12:00','2015-11-01 15:55:00',244.19,222,222,2,1),(10,'2015-11-01 16:36:00','2015-11-01 18:03:00',244.19,222,222,2,1),(11,'2015-11-02 08:22:00','2015-11-02 09:29:00',145.96,222,222,3,1),(12,'2015-11-02 10:45:00','2015-11-02 11:59:00',145.96,222,222,3,1),(13,'2015-11-02 12:18:00','2015-11-02 13:01:00',145.96,222,222,3,1),(14,'2015-11-02 14:12:00','2015-11-02 15:55:00',145.96,222,222,3,1),(15,'2015-11-02 16:36:00','2015-11-02 18:03:00',145.96,222,222,3,1),(16,'2015-11-02 08:22:00','2015-11-02 09:29:00',244.19,222,222,4,1),(17,'2015-11-02 10:45:00','2015-11-02 11:59:00',244.19,222,222,4,1),(18,'2015-11-02 12:18:00','2015-11-02 13:01:00',244.19,222,222,4,1),(19,'2015-11-02 14:12:00','2015-11-02 15:55:00',244.19,222,222,4,1),(20,'2015-11-02 16:36:00','2015-11-02 18:03:00',244.19,222,222,4,1),(21,'2015-11-01 08:55:00','2015-11-01 09:45:00',97.28,222,222,5,1),(22,'2015-11-01 10:40:00','2015-11-01 11:44:00',97.28,222,222,5,1),(23,'2015-11-01 12:26:00','2015-11-01 13:14:00',97.28,222,222,5,1),(24,'2015-11-01 14:19:00','2015-11-01 15:59:00',97.28,222,222,5,1),(25,'2015-11-01 16:42:00','2015-11-01 18:38:00',97.28,222,222,5,1),(26,'2015-11-05 06:20:00','2015-11-05 07:20:00',377.99,222,222,2,1),(27,'2015-11-05 07:15:00','2015-11-05 09:02:00',377.99,222,222,2,1),(28,'2015-11-05 08:18:00','2015-11-05 11:16:00',377.99,222,222,2,1),(29,'2015-11-05 09:12:00','2015-11-05 13:55:00',377.99,222,222,2,1),(30,'2015-11-05 16:50:00','2015-11-05 19:23:00',377.99,222,222,2,1),(31,'2015-11-05 08:55:00','2015-11-05 09:45:00',97.28,222,222,7,2),(32,'2015-11-05 10:40:00','2015-11-05 11:44:00',97.28,222,222,7,2),(33,'2015-11-05 12:26:00','2015-11-05 13:14:00',97.28,222,222,7,2),(34,'2015-11-05 14:19:00','2015-11-05 15:59:00',97.28,222,222,7,2),(35,'2015-11-05 16:42:00','2015-11-05 18:38:00',97.28,222,222,7,2),(36,'2015-11-06 08:55:00','2015-11-06 09:45:00',97.28,222,222,5,2),(37,'2015-11-07 10:40:00','2015-11-07 11:44:00',97.28,222,222,1,2),(38,'2015-11-06 12:26:00','2015-11-06 13:14:00',97.28,222,222,6,1),(39,'2015-11-07 14:19:00','2015-11-07 15:59:00',97.28,222,222,7,3),(40,'2015-11-08 16:42:00','2015-11-08 18:38:00',97.28,222,222,2,5);
+/*!40000 ALTER TABLE `schedules` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-08 23:57:36
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bank
+-- ------------------------------------------------------
+-- Server version	5.6.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` int(11) NOT NULL,
+  `agency` int(11) NOT NULL,
+  `balance` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,87983,489,3779.05),(2,575,63556,122.95),(3,781,88246,15759.21);
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-08 23:57:36
